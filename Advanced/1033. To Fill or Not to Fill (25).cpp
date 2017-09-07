9a018d908abaaa746755e4b90a47b18aa6bba2d5
Baldr <YGM_Baldr@163.com>
2017-09-07 15:18:21 +0800

    17-9-7

--- Advanced/1033. To Fill or Not to Fill (25).cpp
+++ Advanced/1033. To Fill or Not to Fill (25).cpp
@@ -38,3 +38,69 @@ Sample Input 2:
 Sample Output 2:
 The maximum travel distance = 1200.00*/
 
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+const int maxn = 510;
+const int INF = 0xfffffff;
+
+struct station{
+    double price,dis;
+}st[maxn];
+
+bool cmp(station a,station b){
+    return a.dis<b.dis;
+}
+int main() {
+    freopen("C:\\Users\\71000\\CLionProjects\\demo\\data.in", "r", stdin);
+
+    int n;
+    double cmax,d,davg;
+    scanf("%lf%lf%lf%d", &cmax, &d, &davg, &n);
+    for (int i = 0; i < n; ++i) {
+        scanf("%lf%lf", &st[i].price, &st[i].dis);
+    }
+    st[n].price=0,st[n].dis=d;
+    sort(st,st+n,cmp);
+    if(st[0].dis!=0){
+        printf("The maximum travel distance = 0.00\n");
+    } else{
+        int now=0;
+        double ans=0,nowTank=0,MAX=cmax*davg;
+        while (now<n){
+            int k=-1;
+            double priceMin=INF;
+            for (int i = now+1; i <=n&&st[i].dis-st[now].dis<=MAX ; ++i) {
+                if(st[i].price<priceMin){
+                    k=i;
+                    priceMin=st[i].price;
+                    if(priceMin<st[now].price){
+                        break;
+                    }
+                }
+            }
+            if(k==-1) break;
+            double need=(st[k].dis-st[now].dis)/davg;
+            if(priceMin<st[now].price){
+                if(nowTank<need){
+                    ans+=(need-nowTank)*st[now].price;
+                    nowTank=0;
+                } else{
+                    nowTank-=need;
+                }
+            } else{
+                ans+=(cmax-nowTank)*st[now].price;
+                nowTank=cmax-need;
+            }
+            now=k;
+        }
+        if(now==n){
+            printf("%.2f\n", ans);
+        } else{
+            printf("The maximum of travel distance = %.2f\n", st[now].dis+MAX);
+        }
+    }
+    
+    return 0;
+}
